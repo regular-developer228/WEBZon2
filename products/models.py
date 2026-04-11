61b50b089099df01ce2d029110435c52e7a0da3d
regular-developer228 <golovkokir22@gmail.com>
2026-04-11 17:14:59 +0200

    added new models

--- products/models.py
+++ products/models.py
@@ -40,9 +40,58 @@ class Review(models.Model):
     text = models.TextField()
     rating = models.IntegerField(blank=True, null=True)
 
+# ================================================
+
+
+# korzyna, zberigaetsa inforamjca v brauzere
+class CartItem(models.Model):
+    # session_key - kluch sessii brauzera
+    session_key = models.CharField(
+        max_length=50,
+        verbose_name="Session key"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name="Item"
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name="Items quantity"
+    )
+
+    added_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Added at"
+    )
+
+    class Meta:
+        verbose_name="Cart item"
+        verbose_name_plural="Cart"
+        unique_together=["session_key", "product"]
+
+    def __str__(self):
+        return f'{self.product.name} x{self.quantity}'  # potato x4
+
+    @property
+    def total_price(self):
+        """Price of this item (price * quantity)"""
+        return self.product.price * self.quantity
+
+# zakaz
+# nomer zakaza i informacja pro pokupcya
+# sozdayetsa posle potverzhdenia oplaty
 class Order(models.Model):
     product_list = []
     order_number = models.CharField(max_length=50, unique=True, verbose_name='Order number')
     customer_name = models.CharField(max_length=200, verbose_name='Customer name')
     customer_email = models.EmailField(max_length=100, verbose_name='Customer email')
     customer_phone = models.CharField(max_length=20, blank=True, verbose_name='Customer phone')
+
+
+# tovary v zakaze
+# schto i v kakom kolichestve zakazano
+class OrderItem(models.Model):
+    pass
